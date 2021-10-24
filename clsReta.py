c1151bd457ad8752686ee65ac55b4917a265f4fd
Rodrigo Neves <92616925+rodr1goJ@users.noreply.github.com>
2021-10-24 18:23:21 -0300

    Update clsReta.py
    Commit2

--- clsReta.py
+++ clsReta.py
@@ -3,6 +3,9 @@ class Retangulo:
         self.comprimento = float(comprimento)
         self.largura = float(largura)
 
+    def metMuda(self, LadoA, LadoB):
+        self.comprimento = LadoA
+        self.largura = LadoB
 
     def calc_Size(self):
         area = self.largura * self.comprimento
@@ -11,3 +14,10 @@ class Retangulo:
     def calc_peri(self):
         perimetro = 2 * self.largura + 2 * self.comprimento
         return perimetro
+
+    def calc_roda(self):
+        if self.comprimento < self.largura:
+            Maior = self.largura
+        else: Maior = self.comprimento
+        print(Maior)
+        return Maior
